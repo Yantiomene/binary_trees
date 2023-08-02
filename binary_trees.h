38216e18c10df2a3c12a3205847453ba7ac891b8
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-08-02 10:22:57 -0700

    [Update] Adding a is_complete function

--- binary_trees.h
+++ binary_trees.h
@@ -60,5 +60,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 void traverse_level(const binary_tree_t *tree, int lev, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+int is_complete(const binary_tree_t *tree, size_t index, size_t n_node);
 
 #endif
